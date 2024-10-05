db401b6d05623f09eecbf5587518bff6ba7958c8
Gökhan TOPUZ <gokhan061@msn.com>
2024-10-05 19:47:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,16 +45,15 @@ def blackjack_player(card):
     if rank in ["Jack","Queen","King"]:
         return 10
     elif rank == "Ace":
-        player_value = int(input("Choose value (1 or 11): "))
-        if player_value == 1:
-            return 1
-        elif player_value == 11:
-            return 11
-        else:
-            print("Wrong value. Choose right.")
-            while player_value != 1 or player_value != 11:
+        while True:
+            try:
                 player_value = int(input("Choose value (1 or 11): "))
-                return player_value
+                if player_value in [1, 11]:
+                    return player_value
+                else:
+                    print("Invalid choice. Please choose 1 or 11.")
+            except ValueError:
+                print("Please enter a number (1 or 11).")
     else:
         return int(rank)
 
@@ -73,14 +72,8 @@ def game():
           "BLACK JACK\n" +
           "===========\n")
 
-    # Calculating The Dealer's hand value for blackjack
-    while True:
-        dealer_hand = draw_hand(2)
-        if dealer_hand.splitlines()[0] != "Ace":
-            break
-        elif dealer_hand.splitlines()[1] == "Ace":
-            continue
-
+    # Dealer's hand calculation
+    dealer_hand = draw_hand(2)
     print("---------------------")
     print("Dealer:")
     print("1. ", dealer_hand.splitlines()[0])
@@ -88,39 +81,48 @@ def game():
     dealer_total_value = sum([blackjack_dealer(card) for card in dealer_hand.splitlines()])
     print("---------------------\n")
 
-    # Calculating Player's hand value for blackjack
+    # Player's hand calculation
     print("You:")
     player_hand = draw_hand(2)
     print(player_hand)
     player_total_value = sum([blackjack_player(card) for card in player_hand.splitlines()])
     print("Your Total Value:",player_total_value)
 
-    # Player gets New Card
+    # Player's decision loop
     get_player_card = ""
     while player_total_value <= 17:
-        player_answer = input('Hit or Stay: ')
+        player_answer = input('Hit or Stay: ').capitalize()
         if player_answer == "Hit":
             get_player_card = draw_hand(1)
+            print(get_player_card)
             player_total_value += blackjack_player(get_player_card)
             if player_total_value >= 21:
                 break
-        else:
+        elif player_answer == "Stay":
             break
+        else:
+            print("Invalid input, please choose 'Hit' or 'Stay'.")
+
+    # Dealer's decision loop
+    get_dealer_card = ""
+    while dealer_total_value <= 17:
+        if player_total_value > 21:
+            break
+        get_dealer_card = draw_hand(1)
+        dealer_total_value += blackjack_dealer(get_dealer_card)
+        if dealer_total_value > 21:
+            for card in dealer_hand.splitlines():
+                if card == "Ace":
+                    dealer_total_value -= 10
+                    break
+
     print("---------------------\n")
     print("You:")
     print("1. ", player_hand.splitlines()[0])
     print("2. ", player_hand.splitlines()[1])
-    print("3. ", get_player_card)
     print("Your total value:", player_total_value)
     print("---------------------\n")
 
-    #The Dealer gets New Card
-    get_dealer_card = ""
-    while dealer_total_value <= 17:
-        get_dealer_card = draw_hand(1)
-        dealer_total_value += blackjack_dealer(get_dealer_card)
-        if dealer_total_value >= 21:
-            break
 
     print("---------------------\n")
     print("Dealer:")
@@ -133,10 +135,10 @@ def game():
     # The Game Result
     if player_total_value == dealer_total_value:
         print("Draw.")
-    elif player_total_value > dealer_total_value or dealer_total_value >21:
+    elif dealer_total_value < player_total_value <= 21:
         if player_total_value == 21:
             print("You win. Black Jack!")
-        else:
+    elif player_total_value <= 21 < dealer_total_value :
             print("You Win.")
     else:
         print("The Dealer wins")
